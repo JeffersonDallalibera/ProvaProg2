9b5eedf962db08f8f226ba945a7e6d2d0bac8103
Jefferson Dallalibera <jeffersonmigueldallalibera@gmail.com>
2022-11-23 22:18:47 -0300

    add dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO pessoa(nome,cpf,rg,sobrenome) VALUES ('Jefferson','08067270988','00000000','miguel Dallalibera');
+INSERT INTO pessoa(nome,cpf,rg,sobrenome) VALUES ('Carlos','123456789','00000000','giongo');
+INSERT INTO endereco(cep,complemento,numero,logradouro,id_pessoa) VALUES ('123456','casa 1',1,'av',1);
+INSERT INTO endereco(cep,complemento,numero,logradouro,id_pessoa) VALUES ('654321','casa 2',2,'r',2);
